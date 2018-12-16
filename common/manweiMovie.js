528db11a40eeda4b87f418ece7ae1308c3425c69
秦娟 <qinjuan@meituan.com>
2018-12-16 21:05:41 +0800

    添加图片资源和删除无用文件

---
+++ common/manweiMovie.js
@@ -0,0 +1,201 @@
+export const manweiMovieList = [{
+	"title": "蚁人2：黄蜂女现身 Ant-Man and the Wasp",
+  "pic": "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2529389608.jpg",
+	"score": "7.3",
+	"scoreNum": "223284人评价",
+	"director": "佩顿·里德",
+	"actor": "保罗·路德 / 伊万杰琳·莉莉 / 迈克尔·佩纳",
+	"type": "类型: 动作 / 科幻 / 冒险",
+	"country": "制片国家/地区: 美国",
+	"date": "2018"
+}, {
+	"title": "复仇者联盟3：无限战争 Avengers: Infinity War",
+    "pic": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2517753454.jpg",
+	"score": "8.1",
+	"scoreNum": "438193人评价",
+	"director": "安东尼·罗素 / 乔·罗素",
+	"actor": "小罗伯特·唐尼 / 克里斯·海姆斯沃斯 / 克里斯·埃文斯",
+	"type": "类型: 动作 / 科幻 / 奇幻 / 冒险",
+	"country": "制片国家/地区: 美国",
+	"date": "2018"
+}, {
+	"title": "雷神3：诸神黄昏 Thor: Ragnarok",
+    "pic": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2500451074.jpg",
+	"score": "7.4",
+	"scoreNum": "236656人评价",
+	"director": "塔伊加·维迪提",
+	"actor": "克里斯·海姆斯沃斯 / 汤姆·希德勒斯顿 / 凯特·布兰切特",
+	"type": "类型: 动作 / 奇幻 / 冒险",
+	"country": "制片国家/地区: 美国",
+	"date": "2017"
+}, {
+	"title": "蜘蛛侠：英雄归来 Spider-Man: Homecoming",
+    "pic": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2497756471.jpg",
+	"score": "7.3",
+	"scoreNum": "242657人评价",
+	"director": "乔·沃茨",
+	"actor": "汤姆·赫兰德 / 小罗伯特·唐尼 / 玛丽莎·托梅",
+	"type": "类型: 动作 / 科幻 / 冒险",
+	"country": "制片国家/地区: 美国",
+	"date": "2017"
+}, {
+	"title": "银河护卫队2 Guardians of the Galaxy Vol. 2",
+  "pic": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2455261804.jpg",
+	"score": "8.0",
+	"scoreNum": "275949人评价",
+	"director": "詹姆斯·古恩",
+	"actor": "克里斯·帕拉特 / 佐伊·索尔达娜 / 戴夫·巴蒂斯塔",
+	"type": "类型: 动作 / 科幻 / 冒险",
+	"country": "制片国家/地区: 美国",
+	"date": "2017"
+}, {
+	"title": "奇异博士 Doctor Strange",
+    "pic": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2388501883.jpg",
+	"score": "7.6",
+	"scoreNum": "332614人评价",
+	"director": "斯科特·德瑞克森",
+	"actor": "本尼迪克特·康伯巴奇 / 蒂尔达·斯文顿 / 麦斯·米科尔森",
+	"type": "类型: 动作 / 奇幻 / 冒险",
+	"country": "制片国家/地区: 美国",
+	"date": "2016"
+}, {
+	"title": "美国队长3 Captain America: Civil War",
+    "pic": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2332503406.jpg",
+	"score": "7.7",
+	"scoreNum": "283579人评价",
+	"director": "安东尼·罗素 / 乔·罗素",
+	"actor": "克里斯·埃文斯 / 小罗伯特·唐尼 / 斯嘉丽·约翰逊",
+	"type": "类型: 动作 / 科幻 / 冒险",
+	"country": "制片国家/地区: 美国",
+	"date": "2016"
+}, {
+	"title": "蚁人 Ant-Man",
+    "pic": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2266823371.jpg",
+	"score": "7.6",
+	"scoreNum": "264635人评价",
+	"director": "佩顿·里德",
+	"actor": "保罗·路德 / 迈克尔·道格拉斯 / 伊万杰琳·莉莉",
+	"type": "类型: 动作 / 科幻 / 冒险",
+	"country": "制片国家/地区: 美国",
+	"date": "2015"
+}, {
+	"title": "复仇者联盟2：奥创纪元 Avengers: Age of Ultron",
+    "pic": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2237747953.jpg",
+	"score": "7.3",
+	"scoreNum": "284381人评价",
+	"director": "乔斯·韦登",
+	"actor": "小罗伯特·唐尼 / 克里斯·海姆斯沃斯 / 马克·鲁弗洛",
+	"type": "类型: 动作 / 科幻 / 奇幻 / 冒险",
+	"country": "制片国家/地区: 美国",
+	"date": "2015"
+}, {
+	"title": "银河护卫队 Guardians of the Galaxy",
+    "pic": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2198455702.jpg",
+	"score": "8.0",
+	"scoreNum": "308884人评价",
+	"director": "詹姆斯·古恩",
+	"actor": "克里斯·帕拉特 / 佐伊·索尔达娜 / 布莱德利·库珀",
+	"type": "类型: 动作 / 科幻 / 冒险",
+	"country": "制片国家/地区: 美国/英国",
+	"date": "2014"
+}, {
+	"title": "美国队长2 Captain America: The Winter Soldier",
+    "pic": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2174824694.jpg",
+	"score": "7.9",
+	"scoreNum": "269899人评价",
+	"director": "安东尼·罗素 / 乔·罗素",
+	"actor": "克里斯·埃文斯 / 斯嘉丽·约翰逊 / 塞巴斯蒂安·斯坦",
+	"type": "类型: 动作 / 科幻 / 冒险",
+	"country": "制片国家/地区: 美国",
+	"date": "2014"
+}, {
+	"title": "雷神2：黑暗世界 Thor: The Dark World",
+    "pic": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2156839164.jpg",
+	"score": "7.4",
+	"scoreNum": "188332人评价",
+	"director": "阿兰·泰勒",
+	"actor": "克里斯·海姆斯沃斯 / 娜塔莉·波特曼 / 汤姆·希德勒斯顿",
+	"type": "类型: 动作 / 奇幻 / 冒险",
+	"country": "制片国家/地区: 美国",
+	"date": "2013"
+}, {
+	"title": "钢铁侠3 Iron Man 3",
+    "pic": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p1955027201.jpg",
+	"score": "7.7",
+	"scoreNum": "263491人评价",
+	"director": "沙恩·布莱克",
+	"actor": "小罗伯特·唐尼 / 盖·皮尔斯 / 格温妮斯·帕特洛",
+	"type": "类型: 动作 / 科幻",
+	"country": "制片国家/地区: 美国/中国大陆",
+	"date": "2013"
+}, {
+	"title": "复仇者联盟 The Avengers",
+    "pic": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p1524904031.jpg",
+	"score": "8.1",
+	"scoreNum": "397416人评价",
+	"director": "乔斯·韦登",
+	"actor": "小罗伯特·唐尼 / 克里斯·埃文斯 / 斯嘉丽·约翰逊",
+	"type": "类型: 动作 / 科幻 / 奇幻 / 冒险",
+	"country": "制片国家/地区: 美国",
+	"date": "2012"
+}, {
+	"title": "美国队长 Captain America: The First Avenger",
+    "pic": "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p1182659238.jpg",
+	"score": "6.7",
+	"scoreNum": "192544人评价",
+	"director": "乔·庄斯顿",
+	"actor": "克里斯·埃文斯 / 海莉·阿特维尔 / 塞巴斯蒂安·斯坦",
+	"type": "类型: 动作 / 科幻 / 冒险",
+	"country": "制片国家/地区: 美国",
+	"date": "2011"
+}, {
+	"title": "雷神 Thor",
+    "pic": "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2159068249.jpg",
+	"score": "6.8",
+	"scoreNum": "174171人评价",
+	"director": "肯尼思·布拉纳",
+	"actor": "克里斯·海姆斯沃斯 / 娜塔莉·波特曼 / 汤姆·希德勒斯顿",
+	"type": "类型: 动作 / 奇幻 / 冒险",
+	"country": "制片国家/地区: 美国",
+	"date": "2011"
+}, {
+	"title": "无敌浩克 The Incredible Hulk",
+    "pic": "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2243996638.jpg",
+	"score": "6.9",
+	"scoreNum": "107156人评价",
+	"director": "路易斯·莱特里尔",
+	"actor": "爱德华·诺顿 / 丽芙·泰勒 / 蒂姆·罗斯",
+	"type": "类型: 动作 / 科幻 / 惊悚",
+	"country": "制片国家/地区: 美国",
+	"date": "2008"
+}, {
+	"title": "钢铁侠2 Iron Man 2",
+    "pic": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p452861346.jpg",
+	"score": "7.4",
+	"scoreNum": "220777人评价",
+	"director": "乔恩·费儒",
+	"actor": "小罗伯特·唐尼 / 格温妮斯·帕特洛 / 米基·洛克",
+	"type": "类型: 动作 / 冒险 / 科幻",
+	"country": "制片国家/地区: 美国",
+	"date": "2010"
+}, {
+	"title": "绿巨人浩克 Hulk",
+    "pic": "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2407903929.jpg",
+	"score": "6.6",
+	"scoreNum": "64285人评价",
+	"director": "李安",
+	"actor": "艾瑞克·巴纳 / 詹妮弗·康纳利 / 山姆·艾里奥特",
+	"type": "类型: 剧情 / 动作 / 科幻",
+	"country": "制片国家/地区: 美国",
+	"date": "2003"
+}, {
+	"title": "钢铁侠 Iron Man",
+    "pic": "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p725871004.jpg",
+	"score": "8.0",
+	"scoreNum": "289476人评价",
+	"director": "乔恩·费儒",
+	"actor": "小罗伯特·唐尼 / 泰伦斯·霍华德 / 格温妮斯·帕特洛",
+	"type": "类型: 动作 / 冒险 / 科幻 / 惊悚",
+	"country": "制片国家/地区: 美国",
+	"date": "2008"
+}]
---
+++ common/musicConfig.js
@@ -0,0 +1,14 @@
+export const musicRankDate  = [
+  { imgUrl: "http://p1.music.126.net/sBzD11nforcuh1jdLSgX7g==/18740076185638788.jpg",
+    type: '音乐原创榜'
+  },
+  { imgUrl: "http://p1.music.126.net/bZvjH5KTuvAT0YXlVa-RtQ==/109951163326845001.jpg",
+    type: '抖音排行榜'
+  },
+  { imgUrl: "http://p1.music.126.net/GhhuF6Ep5Tq9IEvLsyCN7w==/18708190348409091.jpg",
+    type: '音乐热歌榜'
+  },
+  { imgUrl: "http://p1.music.126.net/N2HO5xfYEqyQ8q6oxCw8IQ==/18713687906568048.jpg",
+    type: '音乐新歌榜'
+  },
+]
