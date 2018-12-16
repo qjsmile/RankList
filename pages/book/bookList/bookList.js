b5dba04dfc125cbf7ca6eaa48396ef10b3208906
秦娟 <qinjuan@meituan.com>
2018-12-16 21:06:29 +0800

    书籍

--- pages/book/bookList/bookList.js
+++ pages/book/bookList/bookList.js
@@ -3,14 +3,21 @@ import { bookList } from "../../../common/bookConfig"
 
 Page({
   data: {
-    bookList: ['a']
+    bookList: []
   },
 
   onLoad: function (options) {
     var that = this
     that.setData({
-      bookList: (bookList.bookList)
+      bookList: (bookList)
     })
+  },
+
+  bookUrl: function () {
+    // console.log(1)
+    // wx.navigateTo({
+    //   url: 'https://book.douban.com/subject/1770782/'
+    // })
   },
 
 })
