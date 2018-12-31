c0fc9dbbd0882d3517c50af116d4e3832f3dd081
秦娟 <qinjuan@meituan.com>
2018-12-31 22:20:44 +0800

    fix

--- pages/game/gameList/gameList.js
+++ pages/game/gameList/gameList.js
@@ -1,66 +1,39 @@
 // pages/game/gameList/gameList.js
+import { iosGameList } from '../../../common/iosGame'
+import { andGameList } from '../../../common/andGame'
+import { onlineGameList } from '../../../common/onlineGame'
+
+const alldata = [iosGameList, andGameList, onlineGameList]
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    gameList: iosGameList,
+    currentData : 0,
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //获取当前滑块的index
+  bindChange:function(e){
+    const that  = this;
+    that.setData({
+      currentData: e.detail.current,
+      gameList: alldata[e.detail.current]
+    })
   },
+  
+  //点击切换，滑块index赋值
+  checkCurrent:function(e){
+    const that = this;
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    if (that.data.currentData === e.target.dataset.current){
+        return false;
+    } else {
+      that.setData({
+        currentData: e.target.dataset.current,
+        gameList: alldata[e.target.dataset.current]
+      })
+    }
   }
 })
