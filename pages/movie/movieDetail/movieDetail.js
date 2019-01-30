b3a536f43ad0c23915b2b2d5cdbaf02ded4950f7
lxb <lixiaobing03@meituan.com>
2019-01-30 21:17:45 +0800

    搜索框

--- pages/movie/movieDetail/movieDetail.js
+++ pages/movie/movieDetail/movieDetail.js
@@ -1,60 +1,26 @@
 // pages/movie/movieDetail.js
 Page({
 
+  /**
+   * 页面的初始数据
+   */
   data: {
-    movie: {},
-  },
-
-  onLoad: function (options) {
-
+    loading: true,
+    movie: {}
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 生命周期函数--监听页面加载
    */
-  onReady: function () {
+  onLoad(options) {
+    let app = getApp()
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    app.request(`https://douban.uieee.com/v2/movie/subject/${options.id}`)
+      .then(d => {
+        this.setData({ movie: d, loading: false });
+        wx.setNavigationBarTitle({ title: d.title });
+      }).catch(e => {
+        console.error(e);
+      })
   }
 })
