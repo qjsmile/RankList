4ef46ea78b6afb2bad863c61828d54fd7a2db65c
秦娟 <qinjuan@meituan.com>
2019-02-26 18:05:42 +0800

    减小图片

--- pages/movie/categoryRank/categoryRank.js
+++ pages/movie/categoryRank/categoryRank.js
@@ -2,6 +2,7 @@
 // pages/movie/categoryRank/categoryRank.js
 import { movieRankDate } from '../../../common/movieFetch'
 import { ShareDesc } from '../../../app'
+
 Page({
   data: {
     movieRankDate: movieRankDate
@@ -10,22 +11,7 @@ Page({
   onLoad: function (options) {
     
   },
-
-  viewMoiveListWithID: function (e) {
-    var data = e.currentTarget.dataset
-    const id = Number(data.id)
-    let url = "../../movie/movieList/movieList?id=" + id
-    wx.navigateTo({
-      url: url
-    })
-  },
   
-  movieSearch: function() {
-    wx.navigateTo({
-      url: '../movieSearch/movieSearch'
-    })
-  },
-
   onShareAppMessage: function () {
     return ShareDesc
   }
